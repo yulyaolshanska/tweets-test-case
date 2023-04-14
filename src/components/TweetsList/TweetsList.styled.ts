87cc985079cb3a7a5a10380b5e153444808e9727
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-04-15 02:37:31 +0300

    LoadMoreButton
    1. styles
    2. LoadMoreButton

--- src/components/TweetsList/TweetsList.styled.ts
+++ src/components/TweetsList/TweetsList.styled.ts
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
-export const TweetList = styled.ul``;
+export const TweetList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 48px;
+  margin-bottom: 30px;
+`;
+
+export const LoadMoreButton = styled.button`
+  display: flex;
+  margin: 0 auto;
+  padding: 14px;
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 1.22;
+  color: #373737;
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  border-radius: 10.3108px;
+  cursor: pointer;
+`;
--- src/components/TweetsList/TweetsList.tsx
+++ src/components/TweetsList/TweetsList.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useGetTweetsQuery } from "../../redux/TweetsApi";
 import { TweetsItem } from "../TweetsItem/TweetsItem";
-import { TweetList } from "./TweetsList.styled";
+import { LoadMoreButton, TweetList } from "./TweetsList.styled";
+
+const TWEETS_PER_PAGE = 10;
+const TWEETS_PER_LOAD = 5;
 
 export const TweetsList: React.FC = () => {
+  const [currentPage] = useState(1);
+  const [displayedTweets, setDisplayedTweets] = useState(TWEETS_PER_PAGE);
+  const { data: tweets, isFetching } = useGetTweetsQuery("");
+  const startIndex = (currentPage - 1) * TWEETS_PER_PAGE;
+  const endIndex = displayedTweets;
+
+  const handleLoadMoreClick = () => {
+    setDisplayedTweets(displayedTweets + TWEETS_PER_LOAD);
+  };
+
   return (
-    <TweetList>
-      <TweetsItem />
-    </TweetList>
+    <>
+      <TweetList>
+        {!isFetching &&
+          tweets
+            ?.slice(startIndex, endIndex)
+            .map(({ id, avatar, tweets, followers }) => (
+              <TweetsItem
+                key={id}
+                id={id}
+                avatar={avatar}
+                tweets={tweets}
+                followers={followers}
+              />
+            ))}
+      </TweetList>
+      {tweets && endIndex < tweets.length && (
+        <LoadMoreButton onClick={handleLoadMoreClick}>Load More</LoadMoreButton>
+      )}
+    </>
   );
 };
