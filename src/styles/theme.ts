210c031d28e31d17f1765181f653241020bf187b
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-04-15 02:33:09 +0300

    global styles,theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  media: {
+    mobile: "(min-width: 320px)",
+    mobileM: "(max-width: 767px)",
+    tablet: "(min-width: 768px)",
+    desktop: "(min-width: 1280px)",
+  },
+};
+
+export default theme;
