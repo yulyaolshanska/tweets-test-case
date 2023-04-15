a4084484a9e4be0952e1220895a5144c33bb1e29
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-04-15 23:37:09 +0300

    Update TweetsItem.styled.ts

--- src/components/TweetsItem/TweetsItem.styled.ts
+++ src/components/TweetsItem/TweetsItem.styled.ts
@@ -16,9 +16,6 @@ export const TweetItem = styled.li`
   width: 380px;
   padding: 28px 36px 36px 36px;
   overflow: hidden;
-  /* &:not(:last-child) {
-    margin-right: 48px;
-  } */
   background: url(${BackgroundImg}),
     linear-gradient(114.99deg, #471ca9 -0.99%, #5736a3 54.28%, #4b2a99 78.99%);
   background-repeat: no-repeat;
