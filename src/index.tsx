b8d4b339305ba0ccd1a8f1dc9e112f68f31f9613
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-04-15 15:23:44 +0300

    try fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./components/App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
@@ -20,4 +20,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+// reportWebVitals();
